72da3c4d1eb1a0a1a0a5553c46bd1879404b97bd
Cyberfn <fernandononatocco@gmail.com>
2024-07-09 20:37:40 -0300

    Refaz a navbar

--- js/menulateral.js
+++ js/menulateral.js
@@ -7,5 +7,24 @@ $(document).ready(function() {
       $('.content').hide();
       var target = $(this).attr('id').replace('-tab', '');
       $('#' + target).show();
+  });
+  
+    var down = false;
+    $('#navbarDropdown').click(function(e){
+      var dropdownMenu = $(this).siblings('.dropdown-menu');
+      if(down){
+        dropdownMenu.removeClass('show');
+        down = false;
+      } else {
+        dropdownMenu.addClass('show');
+        down = true;
+      }
+    });
+
+    $(window).click(function(e) {
+      if (!$(e.target).closest('#navbarDropdown').length) {
+        $('.dropdown-menu').removeClass('show');
+        down = false;
+      }
     });
   });
--- principal.php
+++ principal.php
@@ -2,18 +2,16 @@
 $titulo_pagina = 'Principal';
 $nome_style = 'css/principal.css';
 ?>
-<?php include 'header.php'; ?>
+<?php include 'navbar.php'; ?>
 
 <div class="container mt-4 justify-content-center" >
-    <div id="remedios" class="content text-center" style="margin-left: 135px;">
+    <div id="remedios" class="content text-center">
         <button type="button" class="btn btn-primary">
             <i class="bi bi-capsule"></i>
             Pesquisar Medicação
         </button>
     </div>
 </div>
-
-
 
 <script src="js/cadastro_usuario.js"></script>
 <?php include 'footer.php'; ?>
