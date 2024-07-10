a7df598850d4ed25d662231181ba022e58925f51
Cyberfn <fernandononatocco@gmail.com>
2024-07-10 16:39:37 -0300

    Ajusta arquivos de conexao

--- conexao.php
+++ conexao.php
@@ -1,12 +1,22 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "careca18.";
-$dbname = "plm";
 
-$conexao = mysqli_connect($servername, $username, $password, $dbname);
+require __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$db_host = getenv('DB_HOST');
+$db_port = getenv('DB_PORT');
+$db_database = getenv('DB_DATABASE');
+$db_username = getenv('DB_USERNAME');
+$db_password = getenv('DB_PASSWORD');
+
+$conexao = mysqli_connect($db_host, $db_username, $db_password, $db_database, $db_port);
 
 if (!$conexao) {
     die("Conexão falhou: " . mysqli_connect_error());
+} else {
+    echo "Conexão bem-sucedida!";
 }
-?>
